4d852908e0c14bb1077bd1b6bc343feb96dd88d7
zlinlyre <zlinlyre@163.com>
2020-07-22 20:55:43 +0800

    提交API_20200720-修改run.py

--- API_20200720/run.py
+++ API_20200720/run.py
@@ -18,16 +18,16 @@ from API_20200720.testcases import test_login
 from API_20200720.common import HTMLTestRunnerNew
 from API_20200720.common import contants
 
-# suite = unittest.TestSuite()
-# loader = unittest.TestLoader()
-# suite.addTests(loader.loadTestsFromModule(test_login))
+suite = unittest.TestSuite()
+loader = unittest.TestLoader()
+suite.addTests(loader.loadTestsFromModule(test_login))
 
-discover = unittest.defaultTestLoader.discover(contants.case_file, "test_*.py")
+# discover = unittest.defaultTestLoader.discover(contants.case_file, "test_*.py")
 
 with open(contants.report_dir+'/report.html', 'wb+') as file:
     runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
                                               title="PYTHON15 API TEST REPORT",
                                               description="前程贷API")
-    # runner.run(suite)
-    runner.run(discover)
+    runner.run(suite)
+    # runner.run(discover)
 
